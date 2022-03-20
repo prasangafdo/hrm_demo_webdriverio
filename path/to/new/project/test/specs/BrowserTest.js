7fe8e3ee5d4cf6c023359991ac4655bb19f454ea
Prasanga Fernando <prasangafdz@outlook.com>
2022-03-20 10:58:38 +0530

    Created BrowserTest.js

---
+++ path/to/new/project/test/specs/BrowserTest.js
@@ -0,0 +1,8 @@
+describe('This suite is for testing the Browser', function () {
+    it('Verify the Browser Object', function () {
+        browser.url('https://opensource-demo.orangehrmlive.com/')
+    }
+
+    )
+
+})
