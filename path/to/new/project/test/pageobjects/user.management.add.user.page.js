32a0e810380f2dbe969bd1a27da548760ae6cc61
Prasanga Fernando <prasangafdz@outlook.com>
2022-04-03 20:59:10 +0530

    Update user.management.add.user.page.js

--- path/to/new/project/test/pageobjects/user.management.add.user.page.js
+++ path/to/new/project/test/pageobjects/user.management.add.user.page.js
@@ -6,6 +6,16 @@ class UserManagementAddUserPage extends Page{
     get lblAddUser(){
         return $("//h1[@id='UserHeading' and text()='Add User']");
     }
+
+    get txtEmployeeName(){
+        return $('#systemUser_employeeName_empName');
+    }
+
+    get txtUsername(){
+        return $('#systemUser_userName');
+    }
+
+
 }
 
 //td[@class='left']/a
