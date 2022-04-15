b15088229bfb421d231c600749d4b06acb0d99a6
Prasanga Fernando <prasangafdz@outlook.com>
2022-04-15 11:43:39 +0530

    Completed 'Verify the Admin can add a new Admin user' test case

--- path/to/new/project/test/specs/UserManagementTest.js
+++ path/to/new/project/test/specs/UserManagementTest.js
@@ -54,14 +54,24 @@ describe('HRM Demo application - Verify User Management Screen', () => {//Scenar
     //     //Add a verification point
     // });
 
-    it('Verify the Admin can add a new Admin user', async() =>{
+    it('Verify the Admin can add a new Admin user', async () => {
         await LoginPage.open();
-            await LoginPage.login('Admin', 'admin123');
-            await expect(LandingPage.lblHRMDemoLogo).toBeExisting();
+        await LoginPage.login('Admin', 'admin123');
+        await expect(LandingPage.lblHRMDemoLogo).toBeExisting();
         await LandingPage.lnkAdmin.click();
         await UserManagement.btnAdd.click();
         await expect(UserManagementAddUser.lblAddUser).toBeDisplayed();
+        await UserManagementAddUser.txtEmployeeName.setValue('A');//Pass value using tab
+        await browser.pause(3000);
+        await browser.keys('Tab');
+        await UserManagementAddUser.txtUsername.setValue('autAdminEnabled01');
+        UserManagementAddUser.txtPassword.setValue('test12345');
+        UserManagementAddUser.txtConfirmPassword.setValue('test12345');
         await UserManagementAddUser.selectAdminFromDropDown();
+        await browser.pause(5000);
+        UserManagementAddUser.btnSave.click();
+        await browser.pause(5000);
+        expect(UserManagementAddUser.lblSaveSuccessMessage).toHaveText('Successfully Saved');
         await browser.pause(6000);
     });
 });
