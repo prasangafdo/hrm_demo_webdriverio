1175fdecbf5ddee0a43f3ccd1907d135f4bed2c0
Prasanga Fernando <prasangafdz@outlook.com>
2022-04-29 13:41:57 +0530

    Create user.management.edit.user.page.js

---
+++ path/to/new/project/test/pageobjects/user.management.edit.user.page.js
@@ -0,0 +1,48 @@
+const Page = require('./page');
+
+
+class UserManagementAddUserPage extends Page{
+
+//     get lblAddUser(){
+//         return $("//h1[@id='UserHeading' and text()='Add User']");
+//     }
+//     get txtEmployeeName(){
+//         return $('#systemUser_employeeName_empName');
+//     }
+//     get txtUsername(){
+//         return $('#systemUser_userName');
+//     }
+//     get txtPassword(){
+//         return $('#systemUser_password');
+//     }
+//     get txtConfirmPassword(){
+//         return $('#systemUser_confirmPassword');
+//     }
+//     get btnSave(){
+//         return $('#btnSave'); //Using id locator
+//     }
+//     get drpDwnUserRole(){
+//         return $('#systemUser_userType');
+//     }
+//     get lblSaveSuccessMessage(){
+//         return $("//div[@class='message success fadable']");
+//     }
+//     get drpDwnStatus(){
+//         return $('#systemUser_status');
+//     }
+//
+//     async selectAdminFromDropDown(){
+//         console.log('------------------------------------',await this.drpDwnUserRole.getValue())
+//         await this.drpDwnUserRole.selectByAttribute('value','1'); //1 = Admin
+//         console.log('------------------------------------',await this.drpDwnUserRole.getValue())
+//     }
+//
+//     async selectDisabledFromDropDown(){
+//         await this.drpDwnStatus.selectByVisibleText('Disabled');
+//     }
+//
+// }
+
+//td[@class='left']/a
+
+module.exports = new UserManagementAddUserPage()
