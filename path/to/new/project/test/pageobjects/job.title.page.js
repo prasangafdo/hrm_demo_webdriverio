f6b94adcd7bbebe34567b606d15de46b3351bd37
Prasanga Fernando <prasangafdz@outlook.com>
2022-06-01 11:24:26 +0530

    Update job.title.page.js
    Added element locators

---
+++ path/to/new/project/test/pageobjects/job.title.page.js
@@ -0,0 +1,14 @@
+const Page = require('../pageobjects/page')
+
+class JobTitlePage extends Page{
+
+    get lblJobTitle(){
+        return $("//h1[text()='Job Titles']")
+    }
+    get btnAdd(){
+        return $('#btnAdd');
+    }
+    get btnDelete(){
+        return $('#btnDelete');
+    }
+}
