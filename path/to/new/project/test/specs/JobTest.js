6f38c96a32e9f4624a15f6af4d4703146706192c
Prasanga Fernando <prasangafdz@outlook.com>
2022-06-01 11:31:46 +0530

    Added navigation to job titles screen

--- path/to/new/project/test/specs/JobTest.js
+++ path/to/new/project/test/specs/JobTest.js
@@ -1,3 +1,6 @@
+const LoginPage = require('../pageobjects/login.page')
+const Job = require('../functions/Job')
+
 
 describe('Verify the functionality of the Job screen', ()=>{
     it('Verify the UI components of the Job screen',async ()=>{
@@ -5,6 +8,8 @@ describe('Verify the functionality of the Job screen', ()=>{
         if(await LoginPage.inputUsername.isDisplayed()){ //Handling the login to check whether the user has already logged into the system or not
             await LoginPage.login('Admin', 'admin123');
         }
+        await Landing.navigateToJobs();
+        //Add navigation to job titles
 
 
     })
