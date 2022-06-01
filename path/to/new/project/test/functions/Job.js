4a6c70332be2580a822e43d60e96cfec798638bb
Prasanga Fernando <prasangafdz@outlook.com>
2022-06-01 11:32:03 +0530

    Added async functions

--- path/to/new/project/test/functions/Job.js
+++ path/to/new/project/test/functions/Job.js
@@ -1,1 +1,14 @@
-// const LandingPage = require('../pageobjects/landing.page');
+const LandingPage = require('../pageobjects/landing.page');
+const JobTitles = require('../pageobjects/job.title.page');
+
+class Job {
+    async isJobTitleTopicDisplaying(){
+        return JobTitles.lblJobTitle.isDisplayed()
+    }
+    async isAddButtonDisplaying(){
+        return JobTitles.btnAdd.isDisplayed()
+    }
+    async isDeleteButtonDisplaying(){
+        return JobTitles.btnDelete.isDisplayed()
+    }
+}
