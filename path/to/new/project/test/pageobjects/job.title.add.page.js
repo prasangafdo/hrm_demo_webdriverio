1e318e9ced7052c79d1495e20a8e5721b14f4190
Prasanga Fernando <prasangafdz@outlook.com>
2022-06-06 13:20:02 +0530

    Update job.title.add.page.js
    Added element locators

--- path/to/new/project/test/pageobjects/job.title.add.page.js
+++ path/to/new/project/test/pageobjects/job.title.add.page.js
@@ -5,6 +5,15 @@ class JobTitleAddPage extends Page{
     get lblAddJobTitlePage(){
         return ('#saveHobTitleHeading')
     }
+    get txtJobTitle(){
+        return ('#jobTitle_jobTitle')
+    }
+    get txtJobDescription(){
+        return ('#jobTitle_jobDescription')
+    }
+    get txtJobTitleNote(){
+        return ('#jobTitle_note')
+    }
 
 }
 
